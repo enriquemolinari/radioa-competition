6356113d93fe5559e2cdd4c1e8d929606804e497
Enrique Pablo Molinari <enrique.molinari@gmail.com>
2020-06-21 16:50:43 -0300

    removing unused imports

--- src/main/java/ar/cpfw/book/competition/persistence/JdbcCompetitionRepository.java
+++ src/main/java/ar/cpfw/book/competition/persistence/JdbcCompetitionRepository.java
@@ -5,7 +5,6 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
